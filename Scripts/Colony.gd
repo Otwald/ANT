bc217905f363bd0bf63842db223b89a98190a821
otwald <hinterlanddev@googlemail.com>
2020-04-18 09:50:27 +0200

    added ability for ants to call gridmap and get a path to a point

--- Scripts/Colony.gd
+++ Scripts/Colony.gd
@@ -10,16 +10,16 @@ export (int) var food
 var objectType = 'colony'
 
 func _init():
-    food = 100
-    self.connect('area_entered', self, 'updateColony')
+	food = 100
+	self.connect('area_entered', self, 'updateColony')
 
 func food_update(change : float ):
-    self.food = self.food + change
-    emit_signal("update_food")
+	self.food = self.food + change
+	emit_signal("update_food")
 
 
 func updateColony(body):
-    if body.objectType == 'ant':
-        body.carry = false
-        body.aim = null
-        food_update(body.capacity)
+	if body.objectType == 'ant':
+		body.carry = false
+		body.path = null
+		food_update(body.capacity)
--- Scripts/Worker_Ant.gd
+++ Scripts/Worker_Ant.gd
@@ -1,23 +1,27 @@
 extends "Root_Ant.gd"
 
 var stats = {
-    'stam' : 100,
-    'health': 50,
-    'cost': 10,
-    'speed': 50,
-    'capacity': 20
+	'stam' : 100,
+	'health': 50,
+	'cost': 10,
+	'speed': 50,
+	'capacity': 20
 }
 
 func _ready():
-    _createAnt(stats)
+	_createAnt(stats)
+	print(self.grid)
 
 func _process(delta):
-    if aim == null:
-        if carry:
-            aim = home
-        else:
-            var resLoc = ressources.get_children()
-            if not resLoc.empty():
-                aim = resLoc[0]
-    else:
-        self.move(aim.position, delta)
+	if self.path:
+		self.followPath(delta)
+	else:
+		if carry:
+			self.path = self.createPath(home)
+			# aim = home
+		else:
+			var resLoc = ressources.get_children()
+			if not resLoc.empty():
+				self.path = self.createPath(resLoc[0])
+				# aim = resLoc[0]
+	# 	self.move(aim.position, delta)
