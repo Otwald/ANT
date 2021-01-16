e84048ec950df48e9b2d254f633c2e3967a0951f
otwald <hinterlanddev@googlemail.com>
2021-01-16 09:27:25 +0100

    rebuild runable version

--- Ressources/Food/Food.gd
+++ Ressources/Food/Food.gd
@@ -8,21 +8,21 @@ signal depleted
 
 
 func _ready():
-    parent.childs.push_back(self)
-    self.position = parent.getPos()
-    print(self.position)
-    self.connect('area_entered', self, 'updateRessource')
-    self.connect('tree_exiting', self, 'deleteRessource')
+	parent.childs.push_back(self)
+	self.position = parent.getPos()
+	print(self.position)
+	self.connect('area_entered', self, 'updateRessource')
+	self.connect('tree_exiting', self, 'deleteRessource')
 
 func updateRessource(body):
-    if body.objectType == 'ant':
-        if body.carry == false:
-            food_level -= body.capacity
-            body.carry = true
+	if body.objectType == 'ant':
+		if body.carry == false:
+			food_level -= body.capacity
+			body.carry = true
 
-    if food_level <= 0:
-        emit_signal('depleted')
-        self.queue_free()
+	if food_level <= 0:
+		emit_signal('depleted')
+		self.queue_free()
 
 func deleteRessource():
-    parent.childs.erase(self)
+	parent.childs.erase(self)
